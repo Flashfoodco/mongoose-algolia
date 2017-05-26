8e6518dcc5bfa7a59168eb0f8a4455f9f59eccd4
Andre Lami <andre.lami.work@gmail.com>
2017-05-26 13:06:46 +0300

    Few changes for dependencies

--- src/index.js
+++ src/index.js
@@ -5,42 +5,42 @@ const clc = require('cli-color');
 
 module.exports = exports = function algoliaIntegration(schema,opts) {
 
-  let options = {
-    indexName: null,
-    appId: null,
-    apiKey: null,
-    selector: null,
-    defaults: null,
-    mappings: null,
-    inflator: null,
-    filter: null,
-    populate: null,
-    dependency: null,
-    debug: false
-  }
+    let options = {
+        indexName: null,
+        appId: null,
+        apiKey: null,
+        selector: null,
+        defaults: null,
+        mappings: null,
+        inflator: null,
+        filter: null,
+        populate: null,
+        dependency: null,
+        debug: false
+    }
 
-  for(let key in opts) {
-    if(key in options) options[key] = opts[key]; //Override default options
-  }
+    for(let key in opts) {
+        if(key in options) options[key] = opts[key]; //Override default options
+    }
 
-  if(!options.indexName) return console.error(clc.cyanBright('[Algolia-sync]'),' -> ',clc.red.bold('Error'),' -> Invalid index name');
-  if(!options.appId || !options.apiKey) return console.error(clc.cyanBright('[Algolia-sync]'),' -> ',clc.red.bold('Error'),' -> Invalid algolia identification');
+    if(!options.indexName) return console.error(clc.cyanBright('[Algolia-sync]'),' -> ',clc.red.bold('Error'),' -> Invalid index name');
+    if(!options.appId || !options.apiKey) return console.error(clc.cyanBright('[Algolia-sync]'),' -> ',clc.red.bold('Error'),' -> Invalid algolia identification');
 
-  const client = algolia(options.appId,options.apiKey);
+    const client = algolia(options.appId,options.apiKey);
 
-  var syncronizer = require('./synchronizer')(options,client);
-  require('./operations').call(schema,options,client,syncronizer);
+    var syncronizer = require('./synchronizer')(options,client);
+    require('./operations').call(schema,options,client,syncronizer);
 
-  if (!options.dependency) {
-      schema.statics.SyncToAlgolia = function(){
-          return require('./synchronize').call(this,options,client);
-      }
+    if (!options.dependency) {
+        schema.statics.SyncToAlgolia = function(){
+            return require('./synchronize').call(this,options,client);
+        }
 
-      schema.statics.SetAlgoliaSettings = function(settings){
-          if(!settings) return console.error(clc.cyanBright('[Algolia-sync]'),' -> ',clc.red.bold('Error'),' -> Invalid settings');
-          return require('./settings').call(this,settings,options,client);
-      }
-  }
+        schema.statics.SetAlgoliaSettings = function(settings){
+            if(!settings) return console.error(clc.cyanBright('[Algolia-sync]'),' -> ',clc.red.bold('Error'),' -> Invalid settings');
+            return require('./settings').call(this,settings,options,client);
+        }
+    }
 
 
 }
--- src/operations.js
+++ src/operations.js
@@ -4,85 +4,84 @@ const utils = require('./utils');
 
 module.exports = function(options,client, synchronizer){
 
-  this.pre('save',function(next) {
-    let isModified = false;
+    this.pre('save',function(next) {
+        let isModified = false;
 
-    let relevantKeys = utils.GetRelevantKeys(this.toJSON(), options.selector);
-    if(relevantKeys && relevantKeys.length) {
-      relevantKeys.forEach(key => {
-        if(this.isModified(key)) isModified = true;
-      });
-    }else{
-      if(this.isModified()) isModified = true;
-    }
-
-    this.wasNew = this.isNew;
-    this.wasModified = isModified;
-    next();
-  });
-
-  this.post('save',function() {
-    let indices = utils.GetIndexName(this,options.indexName);
-
-    var itemsToSync = [];
-
-    if (!options.dependency) {
-        itemsToSync = [this];
-    } else {
-        var dependentItems = options.dependency.onDependencySaved(this);
-        if (!dependentItems) {
-            dependentItems = [];
-        }
-
-        var isArray = dependentItems instanceof Array;
-        if(isArray === false) {
-            dependentItems = [dependentItems];
-        }
-
-        itemsToSync = dependentItems;
-    }
-
-    itemsToSync.forEach(new function (item) {
-        if(indices instanceof Array) {
-            indices.forEach(index => synchronizer.SyncItem(item, client.initIndex(index)));
+        let relevantKeys = utils.GetRelevantKeys(this.toJSON(), options.selector);
+        if(relevantKeys && relevantKeys.length) {
+            relevantKeys.forEach(key => {
+                if(this.isModified(key)) isModified = true;
+        });
         }else{
-            synchronizer.SyncItem(item, client.initIndex(indices));
+            if(this.isModified()) isModified = true;
+        }
+
+        this.wasNew = this.isNew;
+        this.wasModified = isModified;
+        next();
+    });
+
+    this.post('save',function() {
+        let indices = utils.GetIndexName(this,options.indexName);
+
+        var itemsToSync = [];
+
+        if (!options.dependency) {
+            if(indices instanceof Array) {
+                indices.forEach(index => synchronizer.SyncItem(this, client.initIndex(index)));
+            }else{
+                synchronizer.SyncItem(this, client.initIndex(indices));
+            }
+        } else {
+            options.dependency.onDependencySaved(this, OnDependentUpdated(indices));
+
+
         }
     });
-  });
 
-  this.post('remove',function(){
-    let indices = utils.GetIndexName(this,options.indexName);
+    this.post('remove',function(){
+        let indices = utils.GetIndexName(this,options.indexName);
 
-      if (!options.dependency) {
-          if(indices instanceof Array) {
-              indices.forEach(index => synchronizer.RemoveItem(this, client.initIndex(index)));
-          }else{
-              synchronizer.RemoveItem(this, client.initIndex(indices));
-          }
+        if (!options.dependency) {
+            if(indices instanceof Array) {
+                indices.forEach(index => synchronizer.RemoveItem(this, client.initIndex(index)));
+            }else{
+                synchronizer.RemoveItem(this, client.initIndex(indices));
+            }
 
-      } else {
-          var dependentItems = options.dependency.onDependencyDeleted(this);
-          if (!dependentItems) {
-              dependentItems = [];
-          }
+        } else {
+            options.dependency.onDependencyDeleted(this, OnDependentUpdated(indices));
 
-          var isArray = dependentItems instanceof Array;
-          if(isArray === false) {
-              dependentItems = [dependentItems];
-          }
+        }
+    });
 
-          var itemsToSync = dependentItems;
 
-          itemsToSync.forEach(new function (item) {
-              if(indices instanceof Array) {
-                  indices.forEach(index => synchronizer.SyncItem(item, client.initIndex(index)));
-              }else{
-                  synchronizer.SyncItem(item, client.initIndex(indices));
-              }
-          });
-      }
-  });
+    function OnDependentUpdated(indices) {
+
+        return function(dependentItems) {
+            if (!dependentItems) {
+                dependentItems = [];
+            }
+
+            var isArray = dependentItems instanceof Array;
+            if(isArray === false) {
+                dependentItems = [dependentItems];
+            }
+
+            var itemsToSync = dependentItems;
+
+            itemsToSync.forEach(function (item) {
+                item.wasModified = true;
+
+                if(indices instanceof Array) {
+                    indices.forEach(index => synchronizer.SyncItem(item, client.initIndex(index)));
+                }else{
+                    synchronizer.SyncItem(item, client.initIndex(indices));
+                }
+            });
+        }
+
+    }
 
 
 }
